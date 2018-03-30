6a2b7bf8c89a556cf2712fa365b0ea93686ec05c
wan_wansi <576529493@qq.com>
2018-03-30 21:11:35 +0800

    by wansi # 完成section3.PracticeC.java程序

--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -1,12 +1,33 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
+        Map<String,Integer> sameElementSet = new HashMap<String,Integer>();
+        Map<String,Integer> collection3 = new HashMap<String,Integer>();
+        for (String s1:collectionA) {
+            if (collection3.containsKey(s1)) {
+                collection3.put(s1, collection3.get(s1) + 1);
+            } else {
+                collection3.put(s1,1);
+            }
+        }
+        for (String s1:collection3.keySet()) {
+            //boolean flag = false;
+            if (object.containsKey(s1)) {
+                //flag = true;
+                if (collection3.get(s1)>3) {
+                    int sub = collection3.get(s1)/3;
+                    sameElementSet.put(s1,collection3.get(s1)-sub);
+                }
+            }
+            continue;
+        }
+        return sameElementSet;
+    }           
 
-        return null;
-    }
 }
