8ae760edf6096a0755ab5d6b509d6bb72281b405
wan_wansi <576529493@qq.com>
2018-03-30 21:42:06 +0800

    by wansi #完成section3.PracticeD.java程序

--- src/main/java/cn/school/thoughtworks/section3/PracticeD.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeD.java
@@ -1,12 +1,38 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeD {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String,Integer> sameElementSet = new HashMap<String,Integer>();
+        Map<String,Integer> collection3 = new HashMap<String,Integer>();
+        for (String s1:collectionA) {
+            int sum=0;
+            for (int i = 0; i < s1.length();++i) {
+                if((i!=s1.length()-1) && s1.charAt(i)=='-'){
+                     sum=sum*10+s1.charAt(i+1)-'0';
+                }
+            }
+            if (collection3.containsKey(String.valueOf(s1.charAt(0)))) {
+                collection3.put(s1, collection3.get(s1) + sum);
+            } else {
+                collection3.put(s1,1);
+            }
+        }
+        for (String s1:collection3.keySet()) {
+            //boolean flag = false;
+            if (object.containsKey(s1)) {
+                //flag = true;
+                if (collection3.get(s1)>3) {
+                    int sub = collection3.get(s1)/3;
+                    sameElementSet.put(s1,collection3.get(s1)-sub);
+                }
+            }
+        }
+        return sameElementSet;
     }
 }
