deb120c917dc7cd36d10e1778cc64c6cf1a20f5c
wan_wansi <576529493@qq.com>
2018-03-30 18:27:06 +0800

    by wansi #完成PracticeA.java程序

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -5,6 +5,15 @@ import java.util.List;
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> SameElementSet = new ArrayList<String>();
+        for (int i = 0; i < collection1.size();++i) {
+            for (int j = 0; j < collection2.size(); ++j) {
+                if (collection1.get(i)== collection2.get(j)) {
+                    SameElementSet.add(collection1.get(i));
+                    break;
+                }
+            }
+        }
+        return SameElementSet;
     }
 }
