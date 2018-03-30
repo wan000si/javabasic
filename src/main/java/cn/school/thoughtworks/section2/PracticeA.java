c185fadc94669c8a0f49c5b6b021488dd4927066
wan_wansi <576529493@qq.com>
2018-03-30 18:37:47 +0800

    by wansi #完成section2.Practice.java程序

--- src/main/java/cn/school/thoughtworks/section2/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeA.java
@@ -6,6 +6,15 @@ import java.util.Map;
 public class PracticeA {
     Map<String,Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
+        Map<String,Integer> sameElementSet = new HashMap<String, Integer>();
+        for (String s1:collection1) {
+            if (sameElementSet.containsKey(s1)) {
+                sameElementSet.put(s1, sameElementSet.get(s1).intValue() + 1);
+            } else {
+                sameElementSet.put(s1,1);
+            }
+        }
+        return sameElementSet;
 
         return null;
     }
