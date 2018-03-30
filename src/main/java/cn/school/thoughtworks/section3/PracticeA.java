683bb8b04a0070eff9b165d88de4de07f5020a2d
wan_wansi <576529493@qq.com>
2018-03-30 20:49:05 +0800

    by wansi #完成section3.Practice.java程序

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -1,12 +1,22 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeA {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
+        Map<String,Integer> sameElementSet = new HashMap<String, Integer>();
+        for (String s1:collectionA.keySet()) {
+            //boolean flag = false;
+            if (object.containsKey(s1)) {
+                    //flag = true;
+                    sameElementSet.put(s1,collectionA.get(s1).intValue()-1);
+                    break;
+                }
+        }
 
-        return null;
+        return sameElementSet;
     }
 }
