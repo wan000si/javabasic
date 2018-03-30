66742923bd3d9b9936cc0292d4cf9acfb452fd2e
wan_wansi <576529493@qq.com>
2018-03-30 18:30:33 +0800

    by wansi #完成PracticeC.java程序

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -6,6 +6,16 @@ import java.util.Map;
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> SameElementSet = new ArrayList<String>();
+        for (String s2:collection1) {
+            boolean flag = false;
+            for (String s1:collection2.keySet()) {
+                if (s2.equals(collection2.get(s1))) {
+                    flag = true;
+                    break;
+                }
+            }
+        }
+        return SameElementSet;
     }
 }
